8869e5ae3c257eb4ab7e681cfebfdd184ca2220f
knoori-code <knoori3@gmail.com>
2023-01-09 18:18:05 -0500

    initial commit

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function (array) {
+  let firstElement = array[0];
+  return firstElement
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Lighthouse", "Labs"]), "Hello");
