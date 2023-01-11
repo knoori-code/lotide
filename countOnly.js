852b4a210ae4865149b32ac2b923058592fcf883
knoori-code <knoori3@gmail.com>
2023-01-11 17:43:33 -0500

    Initial commit for countOnly

---
+++ countOnly.js
@@ -0,0 +1,53 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// AllItems: an array of strings that we need to look through 
+// itemsToCount: an object specifying what to count
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+
+  for (let item of allItems) {
+    // if (results[item]) {
+    //   results[item] =+ 1;
+    // } else {
+    //   results[item] = 1;
+    // }
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results
+}
+// countOnly given array and object
+// returns object containing counts of what input object lists
+
+
+
+// Test code
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
--- middle.js
+++ middle.js
@@ -34,6 +34,8 @@ return newArray
 }
 
 console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2])) // => [2]
+console.log(middle([1])) // => [2]
 console.log(middle([1, 2, 3, 4, 5])) // => [3]
 console.log(middle([1, 2, 3, 4, 5, 6])) // => [3]
 console.log(middle([1, 2, 3, 4, 5, 6, 7]))
