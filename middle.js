3669a477353308e6cb4f9bc22996fafafa6cc784
knoori-code <knoori3@gmail.com>
2023-01-16 15:26:13 -0500

    Refactored code and removed test cases

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false
-      }
-    }
-  }
-  return true
-}
-
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`🟢🟢🟢Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${array1} !== ${array2}`);
-  }
-}
-
-
 const middle = function (array) {
   // count how many elements that are in array
   let numOfElements = array.length;
@@ -33,14 +13,4 @@ const middle = function (array) {
 return newArray
 }
 
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2])) // => [2]
-console.log(middle([1])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3]
-console.log(middle([1, 2, 3, 4, 5, 6, 7]))
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-assertArraysEqual(middle([1, 2, 3]), [3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6])
+module.exports = middle;
