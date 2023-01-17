35292cdef0574f2c7069e525ed6ff0f55bd169f5
knoori-code <knoori3@gmail.com>
2023-01-16 20:25:31 -0500

    Modified index.js using property value shorthand

--- index.js
+++ index.js
@@ -16,24 +16,43 @@ const takeUntil = require('./takeUntil')
 const without = require('./without')
 
 
-
-
-
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual, 
-  assertObjectsEqual: assertObjectsEqual, 
-  countLetter: countLetter,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual, 
+  assertObjectsEqual, 
+  countLetter,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 }
+
+
+
+
+// module.exports = {
+//   head: head,
+//   tail: tail,
+//   middle: middle,
+//   assertArraysEqual: assertArraysEqual,
+//   assertEqual: assertEqual, 
+//   assertObjectsEqual: assertObjectsEqual, 
+//   countLetter: countLetter,
+//   countOnly: countOnly,
+//   eqArrays: eqArrays,
+//   eqObjects: eqObjects,
+//   findKey: findKey,
+//   findKeyByValue: findKeyByValue,
+//   letterPositions: letterPositions,
+//   map: map,
+//   takeUntil: takeUntil,
+//   without: without
+// }
