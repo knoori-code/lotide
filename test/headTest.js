58058ec23e217b2f7513c08b9219b9a1a8959df2
knoori-code <knoori3@gmail.com>
2023-01-16 19:06:15 -0500

    Added mocha and chai to project, changed headTest.js to use them

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,26 @@
+const assert = require('chai').assert
 const head = require("../head")
-const assertEqual = require("../assertEqual")
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Lighthouse", "Labs"]), "Hello");
+describe("head function", () => {
+
+  it("Returns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1,2,3]), 1)
+  })
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]), 5); 
+  });
+
+  it("returns 'Hello' for [\'Hello\', \'Lighthouse\', \'Labs\']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+
+  it("returns 'Lighthouse' for [\"Lighthouse\", \"Labs\"]", () => {
+    assert.strictEqual(head(["Lighthouse", "Labs"]), "Lighthouse"); 
+  });
+
+})
